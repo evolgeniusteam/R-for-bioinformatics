f7232d4163e565aabde88fe00d609bba3c927c99
weihuachencn <evolgenius.team@gmail.com>
2020-03-06 09:41:19 +0800

    update talk 04

---
+++ test.R
@@ -0,0 +1,18 @@
+
+
+## vector is the basic unit of R ... 
+a <- 100;
+a[1];
+
+b <- "this is a string";
+b[1];
+
+## -- vecterization ...
+x <- c(10,100,1000, 10000);
+( y <- sqrt( x ) * 4 + 10 );
+
+
+long <- 201:210;
+short <- c(1,10);
+
+long/short;
