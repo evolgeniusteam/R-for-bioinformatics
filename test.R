ccef5b9da12a8d68f961f4467bb14d8ca1cad716
weihuachencn <evolgenius.team@gmail.com>
2020-03-25 20:27:37 +0800

    talk09 march 2020

--- test.R
+++ test.R
@@ -1,18 +1,3 @@
+library(tidyverse);
 
-
-## vector is the basic unit of R ... 
-a <- 100;
-a[1];
-
-b <- "this is a string";
-b[1];
-
-## -- vecterization ...
-x <- c(10,100,1000, 10000);
-( y <- sqrt( x ) * 4 + 10 );
-
-
-long <- 201:210;
-short <- c(1,10);
-
-long/short;
+ppi <- read_delim( file = "data/talk06/ppi900.txt.gz", delim = "\t" );
